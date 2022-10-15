b083b79ae1bac1483491b5f16a57f3492c8aff31
EnzeDai <56224431+EnzeDai@users.noreply.github.com>
2022-10-15 22:17:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ template_id = os.environ["TEMPLATE_ID"]
 
 
 def get_weather():
-  print(city);
+  city = "北京"
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
-  print(res);
+  print(res)
   weather = res['data']['list'][0]
   return weather['weather'], math.floor(weather['temp'])
 
